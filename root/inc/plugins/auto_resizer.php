529e12ad1aec12c070367cfc278d1b21d49ec239
Laird Shaw <lairdshaw77@gmail.com>
2020-12-21 23:51:55 +1000

    Fix version.
    Version was not updated from 1.0.0 to 1.0.1
    prior to initial release. Set it to 1.0.2
    for a new, corrective release.

--- root/inc/plugins/auto_resizer.php
+++ root/inc/plugins/auto_resizer.php
@@ -40,10 +40,10 @@ function auto_resizer_info() {
 		'website'       => '',
 		'author'        => 'MyBB Group',
 		'authorsite'    => 'https://mybb.group/',
-		'version'       => '1.0.0',
+		'version'       => '1.0.2',
 		// Constructed by converting each digit of 'version' above into two digits (zero-padded if necessary),
 		// then concatenating them, then removing any leading zero(es) to avoid the value being interpreted as octal.
-		'version_code'  => '10000',
+		'version_code'  => '10002',
 		'guid'          => '',
 		'codename'      => 'auto_resizer',
 		'compatibility' => '18*'
